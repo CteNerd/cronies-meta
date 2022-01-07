b5a0c6e6068511ecd2f93ceb7236f346d17faa16
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2022-01-07 11:04:52 -0600

    its alive!!!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import logo from "./cronies-logo.svg";
+import Box from "./components/Box"
 import "./App.css";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[2.5, 0, 0]} />
+      </Canvas>
         <p>Hard Hat Required. Constructing the Meta.</p>
       </header>
     </div>
